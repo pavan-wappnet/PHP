c7c0a8ff12f2b3ad4377124faae10df978babf60
pavan-wappnet <pavan.wappnet@gmail.com>
2024-02-05 18:25:20 +0530

    main

--- data_types.php
+++ data_types.php
@@ -7,25 +7,25 @@ $lang = array("Python", "JS", "PHP", "HTML", "CSS");   #Type Array
 $null = null;
 
 
-var_dump($x);    #Example1
+var_dump($x);    #Example1     output = int(5)
 echo "<br>";
-var_dump($y);   #Example2
+var_dump($y);   #Example2      output = float(10.5)
 echo "<br>";
-var_dump($z);   #Example3
+var_dump($z);   #Example3      output = bool(true)
 echo "<br>";
-var_dump($lang);    #Example4
+var_dump($lang);    #Example4  output = array(5) { [0]=> string(6) "Python" [1]=> string(2) "JS" [2]=> string(3) "PHP" [3]=> string(4) "HTML" [4]=> string(3) "CSS" }
 echo "<br>";
-var_dump($txt);     #Example5
+var_dump($txt);     #Example5  output = string(12) "Hello world!"
 echo "<br>";
-var_dump($null);    #Example6
+var_dump($null);    #Example6  output = NULL
 echo "<br>";
 
 
 #Example 7 (changing data type)
 $a = 5;
 $a = (string) $a;
-var_dump($a);
-echo "<br>";
+var_dump($a);        #output = string(1) "5"
+echo "<br>";     
 
 #Example of Object
 class Car{
---
+++ operators.php
@@ -0,0 +1,57 @@
+<?php
+$a = 12;
+$b = 6;
+
+#ARITHMETIC OPERATORS
+
+echo "<br>";
+echo $a + $b;        #output = 18
+echo "<br>";
+echo $a - $b;        #output = 6
+echo "<br>";
+echo $a * $b;        #output = 2
+echo "<br>";
+echo $a / $b;        #output = 72
+echo "<br>";
+echo $a % $b;        #output = 0
+echo "<br>";
+echo $a ** $b;       #output = 2985984
+echo "<br>";
+echo "<br>";
+
+#ASSIGNMENT OPERATORS
+$x = 5;
+$x += 10;
+echo $x;    #ouput = 15 and x will be 15
+echo "<br>";
+
+$x -= 12;
+echo $x;     #output = 3 (x)
+echo "<br>";
+
+$x *= 2;
+echo $x;    #output = 6 (x)
+echo "<br>";
+
+$x /= 2;
+echo $x;    #output = 3(x)
+echo "<br>";
+
+$x %= 3;
+echo $x;
+echo "<br>";   #output = 0
+
+#COMPARISON OPERATORS
+# Equal(==) , Identical(===), Not Equal(!=, <>), Not Identical(!==)
+# Greater(>) , Less(<),
+
+
+#INCREMENT/DECREMENT OPERATORS
+#PRE INCREMENT (++$x)
+#POST INCREMENT ($x++)
+#PRE DECREMENT (--$x)
+#POST DECREMENT ($x--)
+
+#Logical OPERATORS
+#AND(&&), OR(||), XOR(xor),
+?>
