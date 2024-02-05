2decd02f2e332e5d3016c42b49201f1347c0d6d2
pavan-wappnet <pavan.wappnet@gmail.com>
2024-02-05 17:33:50 +0530

    main

---
+++ data_types.php
@@ -0,0 +1,19 @@
+<?php
+$txt = "Hello world!";  # Type String
+$x = 5;                        # Type int
+$y = 10.5;    # Type Float
+$z = true; # Type Boolean
+$lang = array("Python", "JS", "PHP", "HTML", "CSS");   #Type Array
+
+
+var_dump($x);
+echo "<br>";
+var_dump($y);
+echo "<br>";
+var_dump($z);
+echo "<br>";
+var_dump($lang);
+echo "<br>";
+var_dump($txt); 
+echo "<br>";
+?>
--- varaibles.php
+++ varaibles.php
@@ -1,4 +1,41 @@
 <?php
-    $var = 5;
-    echo $var;
+$txt = "Hello world!";  # Type String
+$x = 5;                        # Type int
+$y = 10.5;    # Type Float
+$z = true; # Type Boolean
+$lang = array("Python", "JS", "PHP", "HTML", "CSS");   #Type Array
+
+#Example 1
+
+$text = "Pavan";
+echo "My name is $text ", "<br>";
+echo "My name is " . $text . " & Welcome to my page"; 
+
+
+#Example 2
+$a = 5;
+$b = 6;
+
+echo $b - $a, "<br>";
+echo $b + $a, "<br>";
+echo $b * $a, "<br>";
+echo $b / $a, "<br>";
+
+
+#Example 3
+print_r($lang[0]);  
+echo "<br>";
+var_dump($a);       #var_dump shows value with type, same goes for print_r
+
+
+#Example 4
+
+$obj = ([2, 3, 6]);
+echo "<br>";
+var_dump($obj);
+
+#Example 5
+
+$x = $y = $z = "Fruit";
+echo $z;
 ?>
